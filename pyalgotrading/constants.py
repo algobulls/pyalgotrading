42e4c7438091ad35f994f0999aa6b9659bb17cae
guanidene <guanidene@gmail.com>
2020-07-03 19:27:50 +0530

    ABC Version updated to 3.2.0 and dropped support for 3.1.0

--- pyalgotrading/constants.py
+++ pyalgotrading/constants.py
@@ -8,7 +8,7 @@ class AlgoBullsEngineVersion(Enum):
     """
     A class of Versioning Constants
     """
-    VERSION_3_1_0 = '3.1.0'
+    VERSION_3_2_0 = '3.2.0'
 
 
 class AlgoBullsJobStatus(Enum):
