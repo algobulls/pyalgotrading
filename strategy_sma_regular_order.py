7c5f5ba868ae01f489862b2e91108c5a19a4eb8b
akhil <brainzz@gmail.com>
2020-08-07 17:07:41 +0550

    Deployed 89d5bad with MkDocs version: 1.1.2

---
+++ strategy_sma_regular_order.py
@@ -0,0 +1,89 @@
+from pyalgotrading.strategy.strategy_base import StrategyBase
+from pyalgotrading.constants import *
+
+
+class StrategySMARegularOrder(StrategyBase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.timeperiod1 = self.strategy_parameters['timeperiod1']
+        self.timeperiod2 = self.strategy_parameters['timeperiod2']
+
+        self.main_order = None
+
+    def initialize(self):
+        self.main_order = {}
+
+    @staticmethod
+    def name():
+        return 'SMA Regular Order Strategy'
+
+    @staticmethod
+    def versions_supported():
+        return AlgoBullsEngineVersion.VERSION_3_2_0
+
+    def get_crossover_value(self, instrument):
+        hist_data = self.get_historical_data(instrument)
+        sma_x = talib.SMA(hist_data['close'], timeperiod=self.timeperiod_x)
+        sma_y = talib.SMA(hist_data['close'], timeperiod=self.timeperiod_y)
+        crossover_value = self.utils.crossover(sma_x, sma_y)
+        return crossover_value
+
+    def strategy_select_instruments_for_entry(self, candle, instruments_bucket):
+
+        selected_instruments_bucket = []
+        sideband_info_bucket = []
+
+        for instrument in instruments_bucket:
+            crossover_value = self.get_crossover_value(instrument)
+            if crossover_value == 1:
+                selected_instruments_bucket.append(instrument)
+                sideband_info_bucket.append({'action': 'BUY'})
+            elif crossover_value == -1:
+                if self.strategy_mode is StrategyMode.INTRADAY:
+                    selected_instruments_bucket.append(instrument)
+                    sideband_info_bucket.append({'action': 'SELL'})
+
+        return selected_instruments_bucket, sideband_info_bucket
+
+    def strategy_enter_position(self, candle, instrument, sideband_info):
+        if sideband_info['action'] == 'BUY':
+            qty = self.number_of_lots * instrument.lot_size
+            self.main_order[instrument] = \
+                self.broker.BuyOrderRegular(instrument=instrument,
+                                            order_code=BrokerOrderCodeConstants.INTRADAY,
+                                            order_variety=BrokerOrderVarietyConstants.MARKET,
+                                            quantity=qty)
+        elif sideband_info['action'] == 'SELL':
+            qty = self.number_of_lots * instrument.lot_size
+            self.main_order[instrument] = \
+                self.broker.SellOrderRegular(instrument=instrument,
+                                             order_code=BrokerOrderCodeConstants.INTRADAY,
+                                             order_variety=BrokerOrderVarietyConstants.MARKET,
+                                             quantity=qty)
+        else:
+            raise SystemExit(f'Got invalid sideband_info value: {sideband_info}')
+
+        return self.main_order[instrument]
+
+    def strategy_select_instruments_for_exit(self, candle, instruments_bucket):
+        selected_instruments_bucket = []
+        sideband_info_bucket = []
+
+        for instrument in instruments_bucket:
+            if self.main_order.get(instrument) is not None:
+                crossover_value = self.get_crossover_value(instrument)
+                if crossover_value in [1, -1]:
+                    selected_instruments_bucket.append(instrument)
+                    sideband_info_bucket.append({'action': 'EXIT'})
+        return selected_instruments_bucket, sideband_info_bucket
+
+    def strategy_exit_position(self, candle, instrument, sideband_info):
+        if sideband_info['action'] == 'EXIT':
+            self.main_order[instrument].exit_position()
+            self.main_order[instrument] = None
+            return True
+
+        return False
+
