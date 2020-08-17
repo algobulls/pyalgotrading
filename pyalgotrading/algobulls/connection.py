155c2e6a17bb301a277162cc7ada391ff2639364
guanidene <guanidene@gmail.com>
2020-08-17 13:25:13 +0530

    Dropped support for pnl cumulative absolute and percentage for realtrading as the API now supports it

--- pyalgotrading/algobulls/connection.py
+++ pyalgotrading/algobulls/connection.py
@@ -513,17 +513,7 @@ class AlgoBullsConnection:
         """
         # assert (isinstance(broker, AlgoBullsSupportedBrokers) is True), f'Argument broker should be an enum of type {AlgoBullsSupportedBrokers.__name__}'
         assert (isinstance(strategy_code, str) is True), f'Argument "strategy_code" should be a string'
-
-        report = self.get_report(strategy_code=strategy_code, trading_type=TradingType.REALTRADING, report_type=TradingReportType.PNL_TABLE, render_as_dataframe=True, show_all_rows=show_all_rows)
-        if isinstance(report, pd.DataFrame):
-            # To avoid warnings thrown by pandas while adding columns to pnl table. Taken from here: https://stackoverflow.com/a/20627316/565595
-            pd.options.mode.chained_assignment = None  # default='warn'
-
-            _ = report[::-1]        # Reverse the list first as list is in descending order, by timestamp
-            _['pnl_cumulative_absolute'] = pd.to_numeric(_['pnl_absolute']).cumsum()
-            _['pnl_cumulative_percentage'] = _['pnl_cumulative_absolute']/pd.to_numeric(_['entry_price'])/pd.to_numeric(_['exit_quantity'])
-            report = _[::-1]        # Reverse the list to original order
-        return report
+        return self.get_report(strategy_code=strategy_code, trading_type=TradingType.REALTRADING, report_type=TradingReportType.PNL_TABLE, render_as_dataframe=True, show_all_rows=show_all_rows)
 
     def get_realtrading_report_statistics(self, strategy_code):
         """
