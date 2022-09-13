6dadfb82d6ed149c95b8914f5e2fb25478c319ec
hemendra.sakpal <hemendra.sakpal@algobulls.com>
2022-09-13 17:20:32 +0530

    fix/position_decision_contants

--- pyalgotrading/constants.py
+++ pyalgotrading/constants.py
@@ -209,6 +209,5 @@ class DecisionConstants(Enum):
     EXIT_POSITION = 'EXIT_POSITION'
 
 
-
 MESSAGE_REALTRADING_FORBIDDEN = 'Forbidden. This strategy is not approved for RT mode. Email us on support@algobulls.com to get your strategy approved. ' \
                                 'The AlgoBulls support team will guide you with the approval process. Once your strategy is approved, you can run it for RT directly from your AlgoBulls account.'
