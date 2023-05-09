2f238e21b810881f5bcb7f10103de321046ed288
prithviraj-gotepatil-algobulls <prithviraj.gotepatil@algobulls.com>
2023-05-09 19:44:27 +0530

    different variable for PT and BT for pnl table

--- pyalgotrading/algobulls/connection.py
+++ pyalgotrading/algobulls/connection.py
@@ -26,7 +26,8 @@ class AlgoBullsConnection:
         Init method that is used while creating an object of this class
         """
         self.api = AlgoBullsAPI(self)
-        self.pnl_data = None
+        self.backtesting_pnl_data = None
+        self.papertrade_pnl_data = None
 
     @staticmethod
     def get_authorization_url():
@@ -460,8 +461,8 @@ class AlgoBullsConnection:
             # No data available, send back an empty dataframe
             _df = pd.DataFrame(columns=list(column_rename_map.values()))
 
-        self.pnl_data = _df
-        return self.pnl_data
+        self.backtesting_pnl_data = _df
+        return self.backtesting_pnl_data
 
     def get_backtesting_report_statistics(self, strategy_code, mode='quantstats', report='metrics', html_dump=False):
         """
@@ -481,13 +482,13 @@ class AlgoBullsConnection:
         order_report = None
         initial_funds = 1e9  # TODO: Allow this to be customized by the user
 
-        if self.pnl_data is None:
+        if self.backtesting_pnl_data is None:
             self.get_backtesting_report_pnl_table(strategy_code)
         else:
             print('Generating Statistics for already fetched P&L data...')
 
         # get pnl data and cleanup as per quantstats format
-        _returns_df = self.pnl_data[['entry_timestamp', 'pnl_absolute']]
+        _returns_df = self.backtesting_pnl_data[['entry_timestamp', 'pnl_absolute']]
         _returns_df = _returns_df.set_index('entry_timestamp')
         _returns_df["total_funds"] = _returns_df.pnl_absolute.cumsum() + initial_funds
         _returns_df = _returns_df.dropna()
@@ -674,27 +675,98 @@ class AlgoBullsConnection:
         Fetch Paper Trading Profit & Loss details
 
         Args:
-            strategy_code: Strategy code
+            strategy_code: strategy code
             show_all_rows: True or False
 
         Returns:
             Report details
         """
         assert isinstance(strategy_code, str), f'Argument "strategy_code" should be a string'
-        return self.get_report(strategy_code=strategy_code, trading_type=TradingType.PAPERTRADING, report_type=TradingReportType.PNL_TABLE, render_as_dataframe=True, show_all_rows=show_all_rows)
 
-    def get_papertrading_report_statistics(self, strategy_code):
+        # Fetch the data
+        data = self.get_report(strategy_code=strategy_code, trading_type=TradingType.PAPERTRADING, report_type=TradingReportType.PNL_TABLE)
+
+        # Post-processing: Cleanup & converting data to dataframe
+        column_rename_map = OrderedDict([
+            ('strategy.instrument.segment', 'instrument_segment'),
+            ('strategy.instrument.tradingsymbol', 'instrument_tradingsymbol'),
+            ('entry.timestamp', 'entry_timestamp'),
+            ('entry.isBuy', 'entry_transaction_type'),
+            ('entry.quantity', 'entry_quantity'),
+            ('entry.prefix', 'entry_currency'),
+            ('entry.price', 'entry_price'),
+            ('exit.timestamp', 'exit_timestamp'),
+            ('exit.isBuy', 'exit_transaction_type'),
+            ('exit.quantity', 'exit_quantity'),
+            ('exit.prefix', 'exit_currency'),
+            ('exit.price', 'exit_price'),
+            ('pnlAbsolute.value', 'pnl_absolute')
+        ])
+
+        if data:
+            # Generate df from json data & perform cleanups
+            _df = pd.json_normalize(data[::-1])[list(column_rename_map.keys())].rename(columns=column_rename_map)
+            _df[['entry_timestamp', 'exit_timestamp']] = _df[['entry_timestamp', 'exit_timestamp']].apply(pd.to_datetime, format="%Y-%m-%d | %H:%M", errors="coerce")
+            _df['entry_transaction_type'] = _df['entry_transaction_type'].apply(lambda _: 'BUY' if _ else 'SELL')
+            _df['exit_transaction_type'] = _df['exit_transaction_type'].apply(lambda _: 'BUY' if _ else 'SELL')
+            _df["pnl_cumulative_absolute"] = _df["pnl_absolute"].cumsum(axis=0, skipna=True)
+
+        else:
+            # No data available, send back an empty dataframe
+            _df = pd.DataFrame(columns=list(column_rename_map.values()))
+
+        self.papertrade_pnl_data = _df
+        return self.papertrade_pnl_data
+
+    def get_papertrading_report_statistics(self, strategy_code, mode='quantstats', report='metrics', html_dump=False):
         """
         Fetch Paper Trading report statistics
 
         Args:
-            strategy_code: Strategy code
+            strategy_code: strategy code
+            mode: extension used to generate statistics
+            report: format and content of the report
+            html_dump: save it as a html file
 
         Returns:
             Report details
         """
+
         assert isinstance(strategy_code, str), f'Argument "strategy_code" should be a string'
-        return self.get_report(strategy_code=strategy_code, trading_type=TradingType.PAPERTRADING, report_type=TradingReportType.STATS_TABLE, render_as_dataframe=True)
+        order_report = None
+        initial_funds = 1e9  # TODO: Allow this to be customized by the user
+
+        if self.papertrade_pnl_data is None:
+            self.get_papertrading_report_pnl_table(strategy_code)
+        else:
+            print('Generating Statistics for already fetched P&L data...')
+
+        # get pnl data and cleanup as per quantstats format
+        _returns_df = self.papertrade_pnl_data[['entry_timestamp', 'pnl_absolute']]
+        _returns_df = _returns_df.set_index('entry_timestamp')
+        _returns_df["total_funds"] = _returns_df.pnl_absolute.cumsum() + initial_funds
+        _returns_df = _returns_df.dropna()
+
+        # Note: Quantstats has a potential bug. It cannot work with multiple entries having the same timestamp. For now, we are dropping multiple entries with the same entry_timestamp (else the quantstats code below would throw an error)
+        # Suggestion for workaround: For entries with same entry timestamps, we can slightly modify the entry timestamps by adding single-digit microseconds to make them unique
+        _returns_df = _returns_df[~_returns_df.index.duplicated(keep='first')]
+
+        # Extract the final column; note: timestamp is the index so that is available too
+        total_funds_series = _returns_df.total_funds
+
+        # select report type
+        if report == "metrics":
+            order_report = qs.reports.metrics(total_funds_series)
+        elif report == "full":
+            order_report = qs.reports.full(total_funds_series)
+
+        # save as html file
+        if html_dump:
+            all_strategies = self.get_all_strategies()
+            strategy_name = all_strategies.loc[all_strategies['strategyCode'] == strategy_code]['strategyName'].iloc[0]
+            qs.reports.html(total_funds_series, title=strategy_name, output='', download_filename=f'report_{strategy_name}_{time.time():.0f}.html')
+
+        return order_report
 
     def get_papertrading_report_order_history(self, strategy_code):
         """
