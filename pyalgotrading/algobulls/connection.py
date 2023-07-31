8c520d35a9363f20cfe0e319a39703dad7f5f230
prithviraj-gotepatil-algobulls <prithviraj.gotepatil@algobulls.com>
2023-07-31 12:11:44 +0530

    changed default values

--- pyalgotrading/algobulls/connection.py
+++ pyalgotrading/algobulls/connection.py
@@ -919,7 +919,7 @@ class AlgoBullsConnection:
 
         return self.stop_job(strategy_code=strategy_code, trading_type=TradingType.PAPERTRADING)
 
-    def get_papertrading_logs(self, strategy_code, auto_update=False, display_logs_in_auto_update_mode=False):
+    def get_papertrading_logs(self, strategy_code, auto_update=True, display_logs_in_auto_update_mode=True):
         """
         Fetch Paper Trading logs
 
@@ -1068,7 +1068,7 @@ class AlgoBullsConnection:
 
         return self.stop_job(strategy_code=strategy_code, trading_type=TradingType.REALTRADING)
 
-    def get_realtrading_logs(self, strategy_code, auto_update=False, display_logs_in_auto_update_mode=False):
+    def get_realtrading_logs(self, strategy_code, auto_update=True, display_logs_in_auto_update_mode=True):
         """
         Fetch Real Trading logs
 
